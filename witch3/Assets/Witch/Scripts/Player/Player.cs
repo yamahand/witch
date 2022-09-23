0ea5330c96cc5f4ee00720e229e9f8a2dd614045
yamahand <a18293372@gmail.com>
2022-09-24 08:58:51 +0900

    ImtstateMachine導入
    ImtstateMachine導入

--- witch3/Assets/Witch/Scripts/Player/Player.cs
+++ witch3/Assets/Witch/Scripts/Player/Player.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using IceMilkTea.Core;
 
 public partial class Player : MonoBehaviour
 {
+    enum StateEvent
+    {
+        Idle,
+        Jump,
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +19,13 @@ public partial class Player : MonoBehaviour
         _inputActions.Player.Fire.performed += context => Debug.Log("fire performed");
         _inputActions.Player.Fire.canceled += context => Debug.Log("fire canceled");
         _inputActions.Player.Fire.started += context => Debug.Log("fire started");
-        _inputActions.Player.Jump.performed += context => Debug.Log("jump");
+        _inputActions.Player.Jump.performed += context => _stateMachine.SendEvent(StateEvent.Jump);
+
+        _stateMachine = new ImtStateMachine<Player, StateEvent>(this);
+        _stateMachine.AddAnyTransition<IdleState>(StateEvent.Idle);
+        _stateMachine.AddTransition<IdleState, JumpState>(StateEvent.Jump);
+
+        _stateMachine.SetStartState<IdleState>();
     }
 
     // Update is called once per frame
@@ -22,7 +35,50 @@ public partial class Player : MonoBehaviour
 
         var dir = _inputActions.Player.Move.ReadValue<Vector2>();
         transform.Translate(dir * speed * Time.deltaTime);
+
+        _stateMachine.Update();
+
+        Debug.Log(_stateMachine.CurrentStateName);
     }
 
+    private class StateBase : ImtStateMachine<Player, StateEvent>.State { }
+
+    private class IdleState : StateBase
+    {
+        protected override void Enter()
+        {
+
+        }
+
+        protected override void Update()
+        {
+
+        }
+    }
+
+    private class JumpState : StateBase
+    {
+        protected override void Enter()
+        {
+            var rigidBody = Context.GetComponent<Rigidbody2D>();
+            var vel = rigidBody.velocity;
+            vel.y += 10.0f;
+            rigidBody.velocity = vel;
+        }
+
+        protected override void Update()
+        {
+            var rigidBody = Context.GetComponent<Rigidbody2D>();
+            var vel = rigidBody.velocity;
+            if(vel.y == 0.0f)
+            {
+                Context._stateMachine.SendEvent(StateEvent.Idle);
+            }
+        }
+    }
+
+    ImtStateMachine<Player, StateEvent> _stateMachine;
+
     WitchInput _inputActions = null;
+    
 }
